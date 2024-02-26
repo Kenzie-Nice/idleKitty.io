b3315a69b82b4604820c4f912e6e819701c65d00
Kenzie-Nice <157401036+Kenzie-Nice@users.noreply.github.com>
2024-02-26 13:42:14 -0700

    Update myimage.js

--- js/myimage.js
+++ js/myimage.js
@@ -1,21 +1,23 @@
 class myImage
-{
+  
+  {
     constructor(myFilePath, x, y, w, h)
     {
-        this,myFilePath = myFilePath;
-        this.myImageObject = loadImage(this.myFilePath);
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
+      this.myFilePath = myFilePath;
+      this.myImageObject = loadImage(this.myFilePath);
+      this.x = x;
+      this.y = y;
+      this.w = w;
+      this.h = h;
     }
 
+    updateX(x)
+    {
+        this.x = x;
+    }
     draw()
     {
-        image(this.myImageObject, this.x, this.y);
-        "live p5"
-        
+      image(this.myImageObject, this.x, this.y);
     }
-
-
-}
+  }
+  
